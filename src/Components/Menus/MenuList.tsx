1f931d9992476840881d0df1891c3bceffce004a
jennifer hodge <jennifer.hodge@russdarrow.com>
2024-10-31 19:40:22 -0500

    working on menu now...

--- src/Components/Menus/MenuList.tsx
+++ src/Components/Menus/MenuList.tsx
@@ -1,24 +1,21 @@
 import HeaderNavBar from "../Header/HeaderNavBar";
-
-
-
+import cutlery from "/public/cutlery.png"
 import "./menuList.css";
 
-export default function  MenuList ()  {
+export default function MenuList() {
   return (
     <>
-    <div>
-    <HeaderNavBar />
-      Pizza
-    </div>
+      <div>
+        <HeaderNavBar />
+        <div className="Menu-flex-container">
+          <h1 className="menu-name"><span className="bigger">Authentic<br></br></span>Mediterranean</h1>
+          <h1></h1>
 
-
-
-</>
-
-
-
-  )
-};
-
-
+          <div className="menu-image-container">
+          <img src={cutlery} alt="cutleryPic" />
+   </div>
+        </div>
+      </div>
+    </>
+  );
+}
